f8f69d9a9e49dafe9470eafb3038a629cb0b079e
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-12-21 16:45:27 -0500

    (blog) Keltson's feedback

--- blog/themes/foobar/static/javascript/main.js
+++ blog/themes/foobar/static/javascript/main.js
@@ -17,7 +17,7 @@ const makeNewSiteTitle = () => {
     ]
     const RANDOM_FILE_TYPE = VALID_FILE_TYPES[Math.floor(Math.random()*VALID_FILE_TYPES.length)];
     const title = document.getElementById('site-title')
-    title.innerHTML = `i_make_things${RANDOM_FILE_TYPE[0]} <span class="secondary-text">(${RANDOM_FILE_TYPE[1]})</span>`
+    title.innerHTML = `i_make_things${RANDOM_FILE_TYPE[0]}`
 }
 
 (function () {
