abdf77c2c331f8419bb8361ab9750740902fbf84
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-12-22 23:08:03 -0500

    (blog) Small tweaks to various parts of content

--- blog/static/javascript/main.js
+++ blog/static/javascript/main.js
@@ -2,22 +2,22 @@ const makeNewSiteTitle = () => {
     const VALID_FILE_TYPES = [
         [".cpp", "C++"],
         [".css", "CSS"],
-        [".dxf","AutoCAD"],
-        [".html","HTML"],
-        [".json","json"],
-        [".js","JavaScript"],
-        [".pde","Arduino"],
-        [".psd","Photoshop"],
-        [".py","Python"],
-        [".scss","SASS"],
-        [".sketch","Sketch"],
-        [".sldprt","SolidWorks"],
-        [".sh","Shell"],
-        [".dng","Lightroom"],
+        [".dxf", "AutoCAD"],
+        [".html", "HTML"],
+        [".json", "json"],
+        [".js", "JavaScript"],
+        [".pde", "Arduino"],
+        [".psd", "Photoshop"],
+        [".py", "Python"],
+        [".scss", "SASS"],
+        [".sketch", "Sketch"],
+        [".sldprt", "SolidWorks"],
+        [".sh", "Shell"],
+        [".dng", "Lightroom"],
     ]
-    const RANDOM_FILE_TYPE = VALID_FILE_TYPES[Math.floor(Math.random()*VALID_FILE_TYPES.length)];
+    const RANDOM_FILE_TYPE = VALID_FILE_TYPES[Math.floor(Math.random() * VALID_FILE_TYPES.length)];
     const title = document.getElementById('site-title')
-    title.innerHTML = `i_make_things${RANDOM_FILE_TYPE[0]}`
+    title.innerHTML = `travis_bumgarner${RANDOM_FILE_TYPE[0]}`
 }
 
 (function () {
