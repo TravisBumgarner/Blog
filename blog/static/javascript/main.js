947abe0aa30702b64325eed84cf91c4b05ef4768
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-01-09 20:02:50 -0700

    final mobile tweaks

--- blog/static/javascript/main.js
+++ blog/static/javascript/main.js
@@ -1,26 +1,40 @@
 (function makeNewSiteTitle() {
+  const VALID_FILE_NAMES = [
+    "prototype",
+    "test",
+    "sample",
+    "mockup",
+    "demo",
+    "final",
+    "draft",
+  ];
+
   const VALID_FILE_TYPES = [
-    [".cpp", "C++"],
-    [".css", "CSS"],
-    [".dxf", "AutoCAD"],
-    [".html", "HTML"],
-    [".json", "json"],
-    [".js", "JavaScript"],
-    [".pde", "Arduino"],
-    [".psd", "Photoshop"],
-    [".py", "Python"],
-    [".scss", "SASS"],
-    [".sketch", "Sketch"],
-    [".sldprt", "SolidWorks"],
-    [".sh", "Shell"],
-    [".dng", "Lightroom"],
-    [".tsx", "TypeScript and React"],
-    [".nef", "Nikon RAW"],
+    "cpp",
+    "css",
+    "dxf",
+    "html",
+    "json",
+    "js",
+    "pde",
+    "psd",
+    "py",
+    "scss",
+    "sketch",
+    "sldprt",
+    "sh",
+    "dng",
+    "tsx",
+    "nef",
+    "jpeg",
+    "tiff",
   ];
   const RANDOM_FILE_TYPE =
     VALID_FILE_TYPES[Math.floor(Math.random() * VALID_FILE_TYPES.length)];
+  const RANDOM_FILE_NAME =
+    VALID_FILE_NAMES[Math.floor(Math.random() * VALID_FILE_NAMES.length)];
   const title = document.getElementById("site-title");
-  title.innerHTML = `travis_bumgarner${RANDOM_FILE_TYPE[0]}`;
+  title.innerHTML = `tb_${RANDOM_FILE_NAME}.${RANDOM_FILE_TYPE}`;
 })();
 
 (function () {
